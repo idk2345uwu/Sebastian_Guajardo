6ec48f057bc9021f1c9555798ffa09ff6475740a
idk2345uwu <163457466+idk2345uwu@users.noreply.github.com>
2024-06-11 11:42:19 -0400

    fin Parte1
    -Pequeño Cambio de Tematica de la Pagina
    -Listo Agregacion de nuevos Imputs (checkbox, textarea)
    -Nuevas Clases para cambiar Contraste de la Pagina a traves de un Troggle
    -Funciones Solo comentadas sobre lo de que necesito hacerlas
    -Credenciales del Firebase

--- assets/Script.js
+++ assets/Script.js
@@ -3,3 +3,39 @@
     //toggleclass
 
 //}
+
+
+
+cambiarfondo()
+    //getDocumentbyId
+    //toggleclass
+    //fondo_oscuro
+
+
+cambiartitulo()
+    //getDocumentbyId
+    //toggleclass
+    //titulo_oscuro
+
+cambiartituloparrafo()
+    //getDocumentbyId
+    //toggleclass
+    //misionvision_oscuro
+
+
+
+cambiarFuente()
+    //getDocumentbyId
+    //For Incremental
+
+
+
+registrarformulario()
+let eNombre
+let eTelefono
+let eCorreo
+let eDirrecion
+let Fecha
+let Empresa_o_no
+
+
