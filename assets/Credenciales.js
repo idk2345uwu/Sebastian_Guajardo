81e95ca341d367e35a628806700dd21ca8e854f9
idk2345uwu <163457466+idk2345uwu@users.noreply.github.com>
2024-06-11 10:23:34 -0400

    1er Commit

---
+++ assets/Credenciales.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHSgPMaRkJ9-ieSz-O7AcJ-EtFem-u3h8",
+  authDomain: "aaaa-5a0fb.firebaseapp.com",
+  projectId: "aaaa-5a0fb",
+  storageBucket: "aaaa-5a0fb.appspot.com",
+  messagingSenderId: "70615447744",
+  appId: "1:70615447744:web:c36f9c6dcba69e6ee49525",
+  measurementId: "G-SBKG2N1S4Y"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
